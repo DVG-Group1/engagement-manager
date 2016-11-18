aec6128838ff1ad003ec7555d31aa7d6ab336d8b
MAXWELL, ANDREW [AG-Contractor/1000] <andrew.maxwell@monsanto.com>
2016-11-18 14:32:57 -0600

    Clean up
    Removed sample code, consolidated some files.

--- src/Main.js
+++ src/Main.js
@@ -1,66 +1,20 @@
-/**
- * In this file, we create a React component
- * which incorporates components provided by Material-UI.
- */
 import React, {Component} from 'react';
-import { RaisedButton, Dialog, FlatButton } from 'material-ui';
-
-import Menu from './menu';
+import { Drawer, MenuItem } from 'material-ui';
 
 class Main extends Component {
   constructor(props, context) {
     super(props, context);
-
-    this.handleRequestClose = this.handleRequestClose.bind(this);
-    this.handleTouchTap = this.handleTouchTap.bind(this);
-
     this.state = {
-      open: false,
+
     };
   }
 
-  handleRequestClose() {
-    this.setState({
-      open: false,
-    });
-  }
-
-  handleTouchTap() {
-    this.setState({
-      open: true,
-    });
-  }
-
   render() {
-    const standardActions = (
-      <FlatButton
-        label="Ok"
-        primary={true}
-        onTouchTap={this.handleRequestClose}
-      />
-    );
-
     return (
 			<div>
-				<Menu/>
-				<div style={{ textAlign: 'center', paddingTop: 200 }}>
-					<Dialog
-						open={this.state.open}
-						title="Super Secret Password"
-						actions={standardActions}
-						onRequestClose={this.handleRequestClose}
-					>
-						1-2-3-4-5 And what not
-					</Dialog>
-
-					<h1>Material-UI</h1>
-					<h2>example project</h2>
-					<RaisedButton
-						label="Super Secret Password"
-						secondary={true}
-						onTouchTap={this.handleTouchTap}
-					/>
-				</div>
+				<Drawer open={true}>
+					<MenuItem>Risk Assessment</MenuItem>
+				</Drawer>
 			</div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
+import React from 'react';
+import { render } from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import './index.css';
+import Main from './Main';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
-
-import React from 'react';
-import { render } from 'react-dom';
-
-import App from './App';
-import './index.css';
-
-render(
-  <App />,
-  document.getElementById('root')
+const App = () => (
+  <MuiThemeProvider>
+    <Main />
+  </MuiThemeProvider>
 );
+
+render(<App />, document.getElementById('root'));
